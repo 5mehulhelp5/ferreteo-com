f39aa5ee475ff7bccf3d4f4bb6532415cede163b
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:26:38 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Model/Config/Backend/ZoomUrl.php
+++ app/code/Acx/ZoomEnvios/Model/Config/Backend/ZoomUrl.php
@@ -16,18 +16,18 @@ use Magento\Framework\Exception\ValidatorException;
  */
 class ZoomUrl extends Value
 {
-    /**
-     * @inheritdoc
-     */
-    function beforeSave()
-    {
-        // phpcs:ignore Magento2.Functions.DiscouragedFunction
-        $host = parse_url((string)$this->getValue(), \PHP_URL_HOST);
+	/**
+	 * @inheritdoc
+	 */
+	function beforeSave()
+	{
+		// phpcs:ignore Magento2.Functions.DiscouragedFunction
+		$host = parse_url((string)$this->getValue(), \PHP_URL_HOST);
 
-        if (!empty($host) && !preg_match('/(?:.+\.|^)zoomenvios\.com$/i', $host)) {
-            throw new ValidatorException(__('ZoomEnvios API endpoint URL\'s must use zoomenvios.com'));
-        }
+		if (!empty($host) && !preg_match('/(?:.+\.|^)zoomenvios\.com$/i', $host)) {
+			throw new ValidatorException(__('ZoomEnvios API endpoint URL\'s must use zoomenvios.com'));
+		}
 
-        return parent::beforeSave();
-    }
+		return parent::beforeSave();
+	}
 }
