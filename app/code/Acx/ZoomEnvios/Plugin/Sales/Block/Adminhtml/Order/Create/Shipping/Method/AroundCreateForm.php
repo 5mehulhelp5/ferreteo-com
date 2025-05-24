16db92715fced749c410fbd6b982e8e7a1075625
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:28:38 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Plugin/Sales/Block/Adminhtml/Order/Create/Shipping/Method/AroundCreateForm.php
+++ app/code/Acx/ZoomEnvios/Plugin/Sales/Block/Adminhtml/Order/Create/Shipping/Method/AroundCreateForm.php
@@ -9,37 +9,37 @@ use Acx\ZoomEnvios\Helper\Data;
 
 class AroundCreateForm
 {
-    /**
-     * @var Data
-     */
-    protected $helper;
-    /**
-     * AfterCreateForm constructor.
-     * @param Data $helper
-     */
-    function __construct(
-        Data $helper
-    ) {
-        $this->helper = $helper;
-    }
+	/**
+	 * @var Data
+	 */
+	protected $helper;
+	/**
+	 * AfterCreateForm constructor.
+	 * @param Data $helper
+	 */
+	function __construct(
+		Data $helper
+	) {
+		$this->helper = $helper;
+	}
 
-    /**
-     * @param Form $subject
-     * @param $result
-     * @return bool
-     */
-    function aroundIsMethodActive(Form $subject, callable $proceed, $code)
-    {
-        $storeId = $subject->getAddress()->getQuote()->getStoreId();
-        $selectStore = $this->helper->getPreSelect($storeId);
-        $getActive = $subject->getActiveMethodRate();
-        if (!$getActive) {
-            if ($selectStore) {
-                if ($code == "zoomenvios") {
-                    return true;
-                }
-            }
-        }
-        return $proceed($code);
-    }
+	/**
+	 * @param Form $subject
+	 * @param $result
+	 * @return bool
+	 */
+	function aroundIsMethodActive(Form $subject, callable $proceed, $code)
+	{
+		$storeId = $subject->getAddress()->getQuote()->getStoreId();
+		$selectStore = $this->helper->getPreSelect($storeId);
+		$getActive = $subject->getActiveMethodRate();
+		if (!$getActive) {
+			if ($selectStore) {
+				if ($code == "zoomenvios") {
+					return true;
+				}
+			}
+		}
+		return $proceed($code);
+	}
 }
