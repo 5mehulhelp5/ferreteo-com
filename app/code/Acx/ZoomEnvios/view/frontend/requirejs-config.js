989ab314a0ccfe59cb43d121c7ec273483a57174
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:33:09 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/view/frontend/requirejs-config.js
+++ app/code/Acx/ZoomEnvios/view/frontend/requirejs-config.js
@@ -4,20 +4,20 @@
  */
 
 var config = {
-    map: {
-       '*': {
-           'Magento_Checkout/js/model/cart/totals-processor/default': 'Acx_ZoomEnvios/js/model/cart/totals-processor/default',
-           'Magento_Checkout/js/model/shipping-save-processor/default': 'Acx_ZoomEnvios/js/model/shipping-save-processor/default',
-           'Magento_Checkout/js/model/shipping-save-processor/payload-extender': 'Acx_ZoomEnvios/js/model/shipping-save-processor/payload-extender',
-           'Magento_Checkout/js/action/set-shipping-information': 'Acx_ZoomEnvios/js/action/set-shipping-information'
-       }
-    }
-    ,
-    config: {
-    	mixins: {
-            'Magento_Checkout/js/view/shipping': {
-                'Acx_ZoomEnvios/js/view/plugin/shipping': true
-            }
-        }
-    }
+	map: {
+	   '*': {
+		   'Magento_Checkout/js/model/cart/totals-processor/default': 'Acx_ZoomEnvios/js/model/cart/totals-processor/default',
+		   'Magento_Checkout/js/model/shipping-save-processor/default': 'Acx_ZoomEnvios/js/model/shipping-save-processor/default',
+		   'Magento_Checkout/js/model/shipping-save-processor/payload-extender': 'Acx_ZoomEnvios/js/model/shipping-save-processor/payload-extender',
+		   'Magento_Checkout/js/action/set-shipping-information': 'Acx_ZoomEnvios/js/action/set-shipping-information'
+	   }
+	}
+	,
+	config: {
+		mixins: {
+			'Magento_Checkout/js/view/shipping': {
+				'Acx_ZoomEnvios/js/view/plugin/shipping': true
+			}
+		}
+	}
 };
