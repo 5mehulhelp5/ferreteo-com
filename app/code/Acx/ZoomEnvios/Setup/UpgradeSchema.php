a5d7bbf4b7bcb2526e6d5332fbc0b849e79aa354
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:29:05 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Setup/UpgradeSchema.php
+++ app/code/Acx/ZoomEnvios/Setup/UpgradeSchema.php
@@ -16,55 +16,55 @@ use Magento\Framework\Setup\SchemaSetupInterface;
  */
 class UpgradeSchema implements UpgradeSchemaInterface {
 
-    /**
-     * {@inheritdoc}
-     */
-    function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context) {
-        $setup->startSetup();
-        if (version_compare($context->getVersion(), '1.1.2') < 0) {
-            //add tax_class_id for sales_order_table table
-            $setup->getConnection()->addColumn(
-                $setup->getTable('quote'),
-                'mailbox_account',
-                [
-                    'type' => 'text',
-                    'nullable' => true,
-                    'comment' => 'Mailbox Account',
-                ]
-            );
+	/**
+	 * {@inheritdoc}
+	 */
+	function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context) {
+		$setup->startSetup();
+		if (version_compare($context->getVersion(), '1.1.2') < 0) {
+			//add tax_class_id for sales_order_table table
+			$setup->getConnection()->addColumn(
+				$setup->getTable('quote'),
+				'mailbox_account',
+				[
+					'type' => 'text',
+					'nullable' => true,
+					'comment' => 'Mailbox Account',
+				]
+			);
 
-            $setup->getConnection()->addColumn(
-                $setup->getTable('quote'),
-                'pickup_office',
-                [
-                    'type' => 'text',
-                    'nullable' => true,
-                    'comment' => 'Pickup Office',
-                ]
-            );
+			$setup->getConnection()->addColumn(
+				$setup->getTable('quote'),
+				'pickup_office',
+				[
+					'type' => 'text',
+					'nullable' => true,
+					'comment' => 'Pickup Office',
+				]
+			);
 
-            $setup->getConnection()->addColumn(
-                $setup->getTable('sales_order'),
-                'mailbox_account',
-                [
-                    'type' => 'text',
-                    'nullable' => true,
-                    'comment' => 'Mailbox Account',
-                ]
-            );
+			$setup->getConnection()->addColumn(
+				$setup->getTable('sales_order'),
+				'mailbox_account',
+				[
+					'type' => 'text',
+					'nullable' => true,
+					'comment' => 'Mailbox Account',
+				]
+			);
 
-            $setup->getConnection()->addColumn(
-                $setup->getTable('sales_order'),
-                'pickup_office',
-                [
-                    'type' => 'text',
-                    'nullable' => true,
-                    'comment' => 'Pickup Office',
-                ]
-            );
+			$setup->getConnection()->addColumn(
+				$setup->getTable('sales_order'),
+				'pickup_office',
+				[
+					'type' => 'text',
+					'nullable' => true,
+					'comment' => 'Pickup Office',
+				]
+			);
 
-        }
-        $setup->endSetup();
-    }
+		}
+		$setup->endSetup();
+	}
 
 }
