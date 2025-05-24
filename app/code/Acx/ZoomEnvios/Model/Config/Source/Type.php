006e7a1f78756ac1c38734594048b085a810d603
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:27:41 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Model/Config/Source/Type.php
+++ app/code/Acx/ZoomEnvios/Model/Config/Source/Type.php
@@ -12,14 +12,14 @@ use Magento\Framework\Data\OptionSourceInterface;
  */
 class Type implements OptionSourceInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    function toOptionArray()
-    {
-        return [
-            ['value' => 'UPS', 'label' => __('United Parcel Service')],
-            ['value' => 'UPS_XML', 'label' => __('United Parcel Service XML')]
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	function toOptionArray()
+	{
+		return [
+			['value' => 'UPS', 'label' => __('United Parcel Service')],
+			['value' => 'UPS_XML', 'label' => __('United Parcel Service XML')]
+		];
+	}
 }
