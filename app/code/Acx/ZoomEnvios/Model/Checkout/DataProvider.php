a232630fc9a488ff0bc0c3e51f7957be89b8c2c6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:26:29 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Model/Checkout/DataProvider.php
+++ app/code/Acx/ZoomEnvios/Model/Checkout/DataProvider.php
@@ -17,86 +17,86 @@ use Magento\Checkout\Model\Cart;
 class DataProvider implements ConfigProviderInterface
 {
 
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $storeManager;
+	/**
+	 * @var \Magento\Store\Model\StoreManagerInterface
+	 */
+	protected $storeManager;
 
-    /**
-     * @var \Acx\ZoomEnvios\Model\ResourceModel\Store\CollectionFactory
-     */
-    protected $storeCollectionFactory;
+	/**
+	 * @var \Acx\ZoomEnvios\Model\ResourceModel\Store\CollectionFactory
+	 */
+	protected $storeCollectionFactory;
 
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $scopeConfig;
+	/**
+	 * @var \Magento\Framework\App\Config\ScopeConfigInterface
+	 */
+	protected $scopeConfig;
 
-    /**
-     * @var Config
-     */
-    protected $configHelper;
-    
-    /**
-     * @var Magento\Checkout\Model\Session;
-     */
-    protected $_checkoutSession;
-    
-    /**
-     *
-     * @var Magento\Checkout\Model\Cart
-     */
-    protected $_cart;
+	/**
+	 * @var Config
+	 */
+	protected $configHelper;
 
-    /**
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param Config $configHelper
-     */
-    function __construct(
-        Config $configHelper,
-        ScopeConfigInterface $scopeConfig,
-        CheckoutSession $checkoutSession,
-        Cart $cart    
-    ) {
-        $this->configHelper = $configHelper;
-        $this->scopeConfig = $scopeConfig;
-        $this->_checkoutSession = $checkoutSession;
-        $this->_cart = $cart;
-    }
+	/**
+	 * @var Magento\Checkout\Model\Session;
+	 */
+	protected $_checkoutSession;
 
-    /**
-     * {@inheritdoc}
-     */
-    function getConfig()
-    {
-        //$city = $this->_checkoutSession->getQuote()-getShippingAddress()->getCity();
-        $city = $this->_cart->getQuote()->getShippingAddress()->getCity();
-        $cityId = $this->configHelper->getCode('origin_city', [strtoupper($city),'code']);
-        $config = [
-            'shipping' => [
-                'select_office' => [
-                    'offices' => $this->getOffices($city)
-                ]
-            ]
-        ];
+	/**
+	 *
+	 * @var Magento\Checkout\Model\Cart
+	 */
+	protected $_cart;
 
-        return $config;
-    }
+	/**
+	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+	 * @param Config $configHelper
+	 */
+	function __construct(
+		Config $configHelper,
+		ScopeConfigInterface $scopeConfig,
+		CheckoutSession $checkoutSession,
+		Cart $cart
+	) {
+		$this->configHelper = $configHelper;
+		$this->scopeConfig = $scopeConfig;
+		$this->_checkoutSession = $checkoutSession;
+		$this->_cart = $cart;
+	}
 
-    function getOffices($city = null)
-    {
-        $officeListStr = $this->configHelper->getCode('origin_city', [strtoupper($city),'office_code']);
-        $officeListArr = explode(',', $officeListStr);
-        $offices = $this->configHelper->getCode('office');
-        $offices_arr = array();
-        $i = 0;
-        foreach ( $offices as $office_name => $office_code) {
-            if(in_array($office_code, $officeListArr)){
-            $offices_arr[$i]["office_code"] = $office_code;
-            $offices_arr[$i]["office_name"] = $office_name;
-            $i++;
-            }
-        }
-        return \Zend_Json::encode($offices_arr);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	function getConfig()
+	{
+		//$city = $this->_checkoutSession->getQuote()-getShippingAddress()->getCity();
+		$city = $this->_cart->getQuote()->getShippingAddress()->getCity();
+		$cityId = $this->configHelper->getCode('origin_city', [strtoupper($city),'code']);
+		$config = [
+			'shipping' => [
+				'select_office' => [
+					'offices' => $this->getOffices($city)
+				]
+			]
+		];
+
+		return $config;
+	}
+
+	function getOffices($city = null)
+	{
+		$officeListStr = $this->configHelper->getCode('origin_city', [strtoupper($city),'office_code']);
+		$officeListArr = explode(',', $officeListStr);
+		$offices = $this->configHelper->getCode('office');
+		$offices_arr = array();
+		$i = 0;
+		foreach ( $offices as $office_name => $office_code) {
+			if(in_array($office_code, $officeListArr)){
+			$offices_arr[$i]["office_code"] = $office_code;
+			$offices_arr[$i]["office_name"] = $office_name;
+			$i++;
+			}
+		}
+		return \Zend_Json::encode($offices_arr);
+	}
 }
