f5c4c67aa3332e51553ef984d1058087b55909d6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-24 13:28:55 +0100

    https://github.com/ferreteo-com/site/issues/5

--- app/code/Acx/ZoomEnvios/Setup/InstallSchema.php
+++ app/code/Acx/ZoomEnvios/Setup/InstallSchema.php
@@ -17,55 +17,55 @@ use Magento\Framework\DB\Ddl\Table;
 class InstallSchema implements InstallSchemaInterface
 {
 
-    /**
-     * {@inheritdoc}
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
-    {
-        $installer = $setup;
-        $installer->startSetup();
+	/**
+	 * {@inheritdoc}
+	 * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+	 */
+	function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+	{
+		$installer = $setup;
+		$installer->startSetup();
 
-        $installer->getConnection()->addColumn(
-            $installer->getTable('quote'),
-            'mailbox_account',
-            [
-                'type' => 'text',
-                'nullable' => true,
-                'comment' => 'Mailbox Account',
-            ]
-        );
+		$installer->getConnection()->addColumn(
+			$installer->getTable('quote'),
+			'mailbox_account',
+			[
+				'type' => 'text',
+				'nullable' => true,
+				'comment' => 'Mailbox Account',
+			]
+		);
 
-        $installer->getConnection()->addColumn(
-            $installer->getTable('quote'),
-            'pickup_office',
-            [
-                'type' => 'text',
-                'nullable' => true,
-                'comment' => 'Pickup Office',
-            ]
-        );
+		$installer->getConnection()->addColumn(
+			$installer->getTable('quote'),
+			'pickup_office',
+			[
+				'type' => 'text',
+				'nullable' => true,
+				'comment' => 'Pickup Office',
+			]
+		);
 
-        $installer->getConnection()->addColumn(
-            $installer->getTable('sales_order'),
-            'mailbox_account',
-            [
-                'type' => 'text',
-                'nullable' => true,
-                'comment' => 'Mailbox Account',
-            ]
-        );
+		$installer->getConnection()->addColumn(
+			$installer->getTable('sales_order'),
+			'mailbox_account',
+			[
+				'type' => 'text',
+				'nullable' => true,
+				'comment' => 'Mailbox Account',
+			]
+		);
 
-        $installer->getConnection()->addColumn(
-            $installer->getTable('sales_order'),
-            'pickup_office',
-            [
-                'type' => 'text',
-                'nullable' => true,
-                'comment' => 'Pickup Office',
-            ]
-        );
+		$installer->getConnection()->addColumn(
+			$installer->getTable('sales_order'),
+			'pickup_office',
+			[
+				'type' => 'text',
+				'nullable' => true,
+				'comment' => 'Pickup Office',
+			]
+		);
 
-        $setup->endSetup();
-    }
+		$setup->endSetup();
+	}
 }
